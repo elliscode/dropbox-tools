ab07e6184b1e332802d349ac5812a5e600d9783a
elliscode <45629640+elliscode@users.noreply.github.com>
2024-02-04 07:13:27 -0500

    file renamer from sd card reader on mac osx

--- file_renamer.py
+++ file_renamer.py
@@ -8,19 +8,27 @@ dated_file_regexp = "(\d{4})-(\d{2})-(\d{2}) \d{2}.\d{2}.\d{2}"
 
 
 def rename_all_files_to_date_created(input_path):
-    destination_path = Path('/Users/danieltownsend/Desktop/date_photos')
+    destination_path = Path('~/Desktop/flip_phone_photos')
     if not os.path.exists(destination_path):
         destination_path.mkdir()
-    for file in os.listdir(input_path):
-        unix_time = os.stat(input_path.joinpath(file)).st_birthtime
-        print(f"{file} -- {unix_time}")
-        destination_file_name = datetime.datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H.%M.%S')
-        destination_full_name = destination_file_name + file[file.rindex('.'):].lower()
-        final_path = destination_path.joinpath(destination_full_name)
-        print(final_path)
-        shutil.copy(input_path.joinpath(file), final_path)
-        print('copied')
+    for currentpath, folders, files in os.walk(input_path):
+        current_path = Path(currentpath)
+        for file in files:
+            if not (file.endswith('.png')\
+                    or file.endswith('.jpg')\
+                    or file.endswith('.jpeg')\
+                    or file.endswith('.mov')\
+                    or file.endswith('.3gp')):
+                continue
+            unix_time = os.stat(current_path.joinpath(file)).st_birthtime
+            print(f"{file} -- {unix_time}")
+            destination_file_name = datetime.datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H.%M.%S')
+            destination_full_name = destination_file_name + file[file.rindex('.'):].lower()
+            final_path = destination_path.joinpath(destination_full_name)
+            print(final_path)
+            shutil.copy(current_path.joinpath(file), final_path)
+            print('copied')
 
 
 if __name__ == "__main__":
-    rename_all_files_to_date_created(Path('/Users/danieltownsend/Desktop/old_aubri_iphone_photos'))
+    rename_all_files_to_date_created(Path('/Volumes/Untitled/'))
